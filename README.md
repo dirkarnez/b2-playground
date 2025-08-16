4808147432ce8c6067ca3161b21839250c2d177d
dirkarnez <smalldirkalex@gmail.com>
2025-08-16 20:32:28 +0800

    - working

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ b2-playground
 =============
 - https://github.com/bfgroup/b2/releases/download/5.3.3/b2-5.3.3.zip
 - \bootstrap.bat
-- ./b2 install --prefix=PREFIX
+- ./b2 install --prefix="b2-installation"
 
 ### Tutorials
 - https://github.com/bfgroup/b2/tree/main/example/hello
---
+++ setup.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+SET PATH=^
+C:\Windows\System32;^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;^
+%DOWNLOADS_DIR%\bison-2.4.1-bin\bin;
+
+cd /d "%USERPROFILE%\Downloads\b2-5.3.3\b2-5.3.3\" &&^
+.\bootstrap.bat mingw &&^
+.\b2 --toolset=gcc install --prefix="b2-installation" &&^
+echo ok
